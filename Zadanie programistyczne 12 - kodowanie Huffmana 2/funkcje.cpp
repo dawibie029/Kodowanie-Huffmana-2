4f2d2a18c5da30f99fc7b224e7c0cbce040b4741
dawibie029 <dawibie029@student.polsl.pl>
2020-11-03 21:54:55 +0100

    Pierwszy commit. Program zawiera potrzebne pliki nagłówkowe z zdefiniowaną strukturą wierzchołek, oraz funkcją odczytującą wiadomość i zapisującą dane w formie listy wierzchołków zawierających znak i ilość wystąpień znaku w tekście oraz zmiennej kod, która zawiera niezmienioną zawartość pliku tekstowego

---
+++ Zadanie programistyczne 12 - kodowanie Huffmana 2/funkcje.cpp
@@ -0,0 +1,45 @@
+#include "funkcje.h"
+#include "struktury.h"
+#include <vector>
+#include <map>
+#include <list>
+#include <string>
+#include <fstream>
+using namespace std;
+/*bool odczytaj_zakodowane(const std::string& adres, std::map<char, std::string>& mapa_wartosci, std::string& kod) {
+	std::ifstream plik(adres);
+	if (plik) {
+		getline(plik, kod, ';');
+			while (!plik.eof()) {
+				char znak;
+				std::string wartosc;
+				plik.get(znak);	//*Pobiera znak
+				plik.get();	//	Pomija symbol oddzielaj¹cy znaki od definicji (dla pliku wejœciowego dowolny, domyœlnie (oraz w pliku wyjœciowym) u¿ywany jest przecinek)
+				getline(plik, wartosc, ',');	//	Wczytuje wartoœæ znaku wczytanego w linijce wy¿ej. Oddzielaj¹c kod od nastêpnego znaku konieczne jest ju¿ u¿ycie przecinka.
+				mapa_wartosci[znak] = wartosc;
+			}
+		return true;
+	}
+	else
+		return false;
+}*/
+bool odczytaj_odkodowane(const string& adres, list<wierzcholek>& wierzcholki, string& kod) {
+	std::ifstream plik(adres);
+	if (plik) {
+		map<char, int> ilosc_znakow;
+		while (!plik.eof()) {
+			char znak;
+			plik.get(znak);
+			ilosc_znakow[znak]++;
+		}
+		for (auto a : ilosc_znakow) {
+			wierzcholek w_pomocniczy;
+			w_pomocniczy.znak = a.first;
+			w_pomocniczy.wartosc = a.second;
+			wierzcholki.push_back(w_pomocniczy);
+		}
+		return true;
+	}
+	else
+		return false;
+}
---
+++ Zadanie programistyczne 12 - kodowanie Huffmana 2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "struktury.h"
+#include "funkcje.h"
+int main()
+{
+    wierzcholek w;
+    std::cout << "wdasdasd" << std::endl;
+}
